956530bfd801474985cedac2bb4821043be29346
Cameron Hummels <chummels@gmail.com>
2019-01-22 13:41:03 -0800

    Updating halo_FIRE.py

--- halo_FIRE.py
+++ halo_FIRE.py
@@ -6,9 +6,16 @@ import matplotlib as mpl
 mpl.use('Agg')
 from matplotlib import pyplot as plt
 import yt
+yt.enable_parallelism()
+import numpy as np
 from yt.frontends.gizmo.api import GizmoDataset
 import sys
+from scipy.signal import gaussian
+from scipy.ndimage import filters
+import os
+import cmocean
 
+# using smoothing length as an analog for the size of the gas region
 def _column_density(field, data):
     return data[('gas', 'H_number_density')] * data[('gas', 'smoothing_length')]
 
@@ -17,7 +24,7 @@ def _column_density(field, data):
 def _cylindrical_radius_kpc(field, data):
     return data[('PartType0', 'cylindrical_radius')] / (3e21)
 
-yt.add_field(("gas","HI_column_density"), function=_column_density, units="cm**-2", sampling_type='particle')
+yt.add_field(("gas","H_I_column_density"), function=_column_density, units="cm**-2", sampling_type='particle')
 yt.add_field(("PartType0","cylindrical_radius_kpc"), function=_cylindrical_radius_kpc, units="cm", sampling_type='particle')
 
 def get_amiga_data(fn):
@@ -63,19 +70,10 @@ def read_amiga_center(amiga_data, output_fn, ds):
     center = amiga_data[halo,1:4][0]
     return ds.arr(center, 'code_length')
 
-def read_amiga_rvir(amiga_data, output_fn, ds):
-    """
-    Figure out the virial radius from amiga's smoothed halo outputs
-    (e.g., halo_0000_smooth.dat) for a given output_fn
-    """
-    output_number = int(os.path.basename(output_fn).split('.')[0][-3:])
-    halo = amiga_data[:,0] == output_number
-    rvir = amiga_data[halo,4][0]
-    return ds.quan(rvir, 'code_length')
-
 if __name__ == '__main__':
 
-    fn = sys.argv[1]
+    with open(sys.argv[1]) as f:
+        fns = [fn.rstrip() for fn in f.readlines()]
     
     #amiga_data = get_amiga_data(sys.argv[2])
     #amiga_data = smooth_amiga(amiga_data)
@@ -84,7 +82,6 @@ if __name__ == '__main__':
         fn_head = fn.split('/')[-1]
         ds = GizmoDataset(fn)
         #c = read_amiga_center(amiga_data, fn, ds)
-        #rvir = read_rockstar_rvir(rockstar_data, ds)
         _, c = ds.find_max('density')
         rvir = ds.quan(30, 'kpc')
         sp = ds.sphere(c, rvir)
@@ -93,12 +90,16 @@ if __name__ == '__main__':
         sp.set_field_parameter("bulk_velocity", bulk_vel)
         ad = ds.all_data()
         ad.set_field_parameter('center', c)
-        ad.set_field_parameter('normal', [0,0,1])
+        ad.set_field_parameter('normal', np.array([1,0,0]))
         ad.set_field_parameter('bulk_velocity', bulk_vel)
-        p = yt.PhasePlot(ad, ('PartType0', 'cylindrical_radius_kpc'), ('PartType0', 'velocity_cylindrical_z'), ('gas', 'HI_column_density'), weight_field=None)
+        p = yt.PhasePlot(ad, ('PartType0', 'cylindrical_radius_kpc'), ('PartType0', 'velocity_cylindrical_z'), ('gas', 'H_I_column_density'), weight_field=('gas', 'ones'))
         p.set_unit(('PartType0', 'cylindrical_radius_kpc'), 'kpc')
         p.set_unit(('PartType0', 'velocity_cylindrical_z'), 'km/s')
         p.set_log(('PartType0', 'velocity_cylindrical_z'), False)
         p.set_xlim(1e1, 1e4)
         p.set_ylim(-1000,1000)
+        p.set_cmap(('gas', 'H_I_column_density'), 'thermal')
+        p.set_zlim(('gas', 'H_I_column_density'), 1e12, 1e21)
+        p.set_xlabel('Impact Parameter (kpc)')
+        p.set_ylabel('Line of Sight Velocity (km/s)')
         p.save()
