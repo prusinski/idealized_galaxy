6e53003649e668c636431d785242fbbabea16b7a
Nikolaus Prusinski <nik@astro.caltech.edu>
2024-07-03 09:00:58 -0700

    240702 part2

--- combine_vbt.py
+++ combine_vbt.py
@@ -17,11 +17,12 @@ bvec = btab['b'].data
 vtab = ascii.read(f'images/KBSS_{num}_0_v.txt')
 vvec = vtab['v'].data
 
-vbts = [fits.open(f'images/KBSS_{num}_{i}.fits')[0].data for i in range(15)]
+vbts = [fits.open(f'images/KBSS_{num}_{i}.fits')[0].data for i in range(29)]
 
 print(bvec.shape, vvec.shape, vbts[0].shape)
 
 avg_vbt = np.nanmean(vbts, axis=0)
+med_vbt = np.nanmedian(vbts, axis=0)
 std_vbt = np.nanstd(vbts, axis=0)
 
 # print(std_vbt)
@@ -38,28 +39,37 @@ for bi, b in enumerate(bvec):
 
 # d.set_np2arr(std_vbt)
 
-fig, ax = plt.subplots(1,2, sharex=True, sharey=True, figsize = (10,8))
+fig, ax = plt.subplots(1,3, sharex=True, sharey=True, figsize = (14,8))
 ax[0].imshow(avg_vbt, aspect='auto',
             # vmin = 1e13, vmax=1e20,
             norm=mpl.colors.LogNorm(vmin = 1e15, vmax=1e25), interpolation='nearest',
             origin='lower', extent = [bvec.min(), bvec.max(), vvec.min(), vvec.max()])
-std = ax[1].imshow(std_vbt, aspect='auto',
+
+ax[1].imshow(med_vbt, aspect='auto',
+            # vmin = 1e13, vmax=1e20,
+            norm=mpl.colors.LogNorm(vmin = 1e15, vmax=1e25), interpolation='nearest',
+            origin='lower', extent = [bvec.min(), bvec.max(), vvec.min(), vvec.max()])
+
+std = ax[2].imshow(std_vbt, aspect='auto',
             # vmin = 1e13, vmax=1e20,,
             norm=mpl.colors.LogNorm(vmin = 1e15, vmax=1e25), interpolation='nearest',
             origin='lower', extent = [bvec.min(), bvec.max(), vvec.min(), vvec.max()])
 
 ax[0].plot(bvec, vcents, 'r.-')
 ax[1].plot(bvec, vcents, 'r.-')
+ax[2].plot(bvec, vcents, 'r.-')
 
-cb = fig.colorbar(std, ax=ax[1], label='$N_\mathrm{HI}$ (cm$^{-2}$)')
+cb = fig.colorbar(std, ax=ax[2], label='$N_\mathrm{HI}$ (cm$^{-2}$)')
 
 ax[0].set_title('Average')
-ax[1].set_title('Standard Deviation')
+ax[1].set_title('Median')
+ax[2].set_title('Standard Deviation')
 
 ax[0].axhline(0, c='k', ls='--', lw = 0.7)
 ax[1].axhline(0, c='k', ls='--', lw = 0.7)
+ax[2].axhline(0, c='k', ls='--', lw = 0.7)
 
 fig.supxlabel('$b$ (kpc)')
 fig.supylabel('$v_\mathrm{LOS}$ (km s$^{-1}$)')
-plt.savefig('MW_halo_vbt_highres.pdf', bbox_inches='tight')
+plt.savefig('MW_halo_vbt_highres_30.pdf', bbox_inches='tight')
 plt.show()
